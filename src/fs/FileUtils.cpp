d54fe9df9e86aeffb90bb4b1b1349f689b407456
rose839 <617418745@qq.com>
2021-04-25 14:44:34 +0800

    add dirname method

--- src/fs/FileUtils.cpp
+++ src/fs/FileUtils.cpp
@@ -1,5 +1,23 @@
+#include <cstring>
+#include <regex>
 #include "FileUtils.h"
 
 std::string FileUtils::dirname(const char *path) {
+    if (path != nullptr && *path != '\0') {
+        return "";
+    }
 
+    if (!::strcmp(path, "/")) {
+        return "/";
+    }
+
+    static const std::regex pattern("(.*)/([^/]+)/?");
+    std::cmatch result;
+    if (std::regex_match(path, result, pattern)) {
+        if (result[1].first == result[1].second) {    // "/path" or "/path/"
+            return "/";
+        }
+        return result[1].str();     // "/path/to", "path/to", or "path/to/"
+    }
+    return ".";
 }
